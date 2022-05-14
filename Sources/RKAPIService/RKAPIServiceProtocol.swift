3e3786adcc385c2258314bbe0054787be0c6cb10
Rakibur Khan <therakiburkhan@gmail.com>
2022-05-14 21:55:16 +0600

    Updated with new methods. iOS 9 and macOS 10.10 methods made optional

--- Sources/RKAPIService/RKAPIServiceProtocol.swift
+++ Sources/RKAPIService/RKAPIServiceProtocol.swift
@@ -12,15 +12,14 @@ import Foundation
  */
 public protocol RKAPIServiceProtocol {
     /**
-     Fetch items with HTTP Get method without any body parameter. Uses asyn/await method of iOS 15
+     Fetch items with HTTP Get method without any body parameter. Uses async/await concurrency of iOS 13
      
      - Parameters:
-     - urlLink: Receives an optional URL
+        - urlLink: Receives an optional URL
      
-     - Throws: An URLError is thrown if urlLink is nil or not a valied URL or server does not provide any response. Also HTTPStatusCode Error (Custom error) can be thrown if server status code is anything but 200...299
+     - Throws: An URLError is thrown if urlLink is nil or not a valied URL or server does not provide any response. Also ``HTTPStatusCode`` Error (Custom error) can be thrown if server status code is anything but 200...299
      
      - Returns: Returns a  ``NetworkResult<T>`` where T is raw Data
-     
      */
     @available(macOS 10.15.0, *)
     @available(iOS 13.0, *)
@@ -28,20 +27,52 @@ public protocol RKAPIServiceProtocol {
     
     
     /**
-     Fetch items with HTTP method with body parameter. Uses asyn/await method of iOS 15
+     Fetch items with HTTP Get method without any body parameter. Uses async/await concurrency of iOS 13
      
      - Parameters:
-     - urlLink: Receives an optional URL
-     - httpMethod: HTTPMethod enum value to send data with that specific method.
-     - body: Optional raw Data for sending to remote server.
-     - jsonData: Accepts a boolean value to determine if HTTP body is in JSON format
+        - urlLink: Receives an `Optional<URL>`
+        - completion: An `@escaping` closure parameter which provides a ``Result<NetworkResult<Data>, Error>`` as return of closure
+     */
+    @available(iOS 9.0, *)
+    @available(macOS 10.10, *)
+    func fetchItems(urlLink: URL?, _ completion: @escaping (Result<NetworkResult<Data>, Error>)-> Void )
+    
+    
+    /**
+     Fetch items with HTTP method with body parameter. Uses asyn/await method of iOS 13
      
-     - Throws: An URLError is thrown if urlLink is nil or not a valied URL or server does not provide any response. Also HTTPStatusCode Error (Custom error) can be thrown if server status code is anything but 200...299
+     - Parameters:
+        - urlLink: Receives an optional URL
+        - httpMethod: ``HTTPMethod`` enum value to send data with that specific method.
+        - body: Optional raw Data for sending to remote server.
+        - jsonData: Accepts a boolean value to determine if HTTP body is in JSON format
+     
+     - Throws: An URLError is thrown if urlLink is nil or not a valied URL or server does not provide any response. Also ``HTTPStatusCode`` Error (Custom error) can be thrown if server status code is anything but 200...299
      
      - Returns: Returns a  ``NetworkResult<T>`` where T is raw Data
-     
      */
     @available(macOS 10.15.0, *)
     @available(iOS 13.0, *)
     func fetchItemsByHTTPMethod(urlLink: URL?, httpMethod: HTTPMethod, body: Data?) async throws -> NetworkResult<Data>
+    
+    
+    /**
+     Fetch items with HTTP method with body parameter. Uses asyn/await method of iOS 13
+     
+     - Parameters:
+        - urlLink: Receives an optional URL
+        - httpMethod: ``HTTPMethod`` enum value to send data with that specific method.
+        - body: Optional raw Data for sending to remote server.
+        - jsonData: Accepts a boolean value to determine if HTTP body is in JSON format
+        - completion: An `@escaping` closure parameter which provides a ``Result<NetworkResult<Data>, Error>`` as return of closure
+     */
+    @available(iOS 9.0, *)
+    @available(macOS 10.10, *)
+    func fetchItemsByHTTPMethod(urlLink: URL?, httpMethod: HTTPMethod, body: Data?, _ completion: @escaping (Result<NetworkResult<Data>, Error>)-> Void )
+}
+
+extension RKAPIServiceProtocol {
+    func fetchItems(urlLink: URL?, _ completion: @escaping (Result<NetworkResult<Data>, Error>)-> Void ) {}
+    
+    func fetchItemsByHTTPMethod(urlLink: URL?, httpMethod: HTTPMethod, body: Data?, _ completion: @escaping (Result<NetworkResult<Data>, Error>)-> Void ) {}
 }
