5d6f537858d376cc7794e6a1fd0a7750cb3b97e1
Rakibur Khan <therakiburkhan@gmail.com>
2022-05-14 21:57:24 +0600

    Implemented new methods of `RKAPIServiceProtocol` and added compatibiity

--- Sources/RKAPIService/RKAPIService.swift
+++ Sources/RKAPIService/RKAPIService.swift
@@ -1,11 +1,22 @@
 import Foundation
-
+/**
+ RKAPIService class. It implements `RKAPIServiceProtocol`. This class serves all the necessary steps to perform a `URLSession` call.
+ */
 public class RKAPIService: RKAPIServiceProtocol {
     
+    /// Static instance of `RKAPIService`. It has the default `URLSessionConfiguration`. `URLSessionDelegate` and `OperationQueue` are both nil.
     public static var shared = RKAPIService(sessionConfiguration: URLSessionConfiguration.default, delegate: nil, queue: nil)
     
     private let session: URLSession
     
+    /**
+     Initializes ``RKAPIService``
+     
+     - Parameters:
+        - sessionConfiguration: Receives ``URLSessionConfiguration`` from ``Foundation``
+        - delegate: Receives an ``Optional<URLSessionDelegate>`` or ``URLSessionDelegate?`` from ``Foundation``
+        - queue: Receiives an ``Optional<OperationQueue>`` or ``OperationQueue`` from ``Foundation``
+     */
     public init(sessionConfiguration: URLSessionConfiguration, delegate: URLSessionDelegate?, queue: OperationQueue?) {
         
         session = URLSession(configuration: sessionConfiguration, delegate: delegate, delegateQueue: queue)
@@ -13,6 +24,35 @@ public class RKAPIService: RKAPIServiceProtocol {
     
     @available(macOS 10.15.0, *)
     @available(iOS 13.0, *)
+    internal func previousVersionURLSession(request: URLRequest) async throws -> (Data, URLResponse) {
+        try await withCheckedThrowingContinuation({ (continuation: CheckedContinuation<(Data, URLResponse), Error>) in
+            session.dataTask(with: request) { data, response, error in
+                if let error = error {
+                 continuation.resume(throwing: error)
+                }
+                else {
+                    if let data = data, let response = response {
+                        continuation.resume(with: .success((data, response)))
+                    }
+                }
+            }
+        })
+    }
+}
+
+@available(macOS 10.15.0, *)
+@available(iOS 13.0, *)
+extension RKAPIService {
+    /**
+     Fetch items with HTTP Get method without any body parameter. Uses async/await concurrency of iOS 13
+     
+     - Parameters:
+        - urlLink: Receives an `Optional<URL>`
+     
+     - Throws: An URLError is thrown if urlLink is nil or not a valied URL or server does not provide any response. Also ``HTTPStatusCode`` Error (Custom error) can be thrown if server status code is anything but 200...299
+     
+     - Returns: Returns a  ``NetworkResult<T>`` where T is raw Data
+     */
     public func fetchItems(urlLink: URL?) async throws -> NetworkResult<Data> {
         guard let url = urlLink else {
             throw URLError(.badURL)
@@ -42,8 +82,19 @@ public class RKAPIService: RKAPIServiceProtocol {
         return NetworkResult(data: rawData, response: status)
     }
     
-    @available(macOS 10.15.0, *)
-    @available(iOS 13.0, *)
+    /**
+     Fetch items with HTTP method with body parameter. Uses asyn/await method of iOS 13
+     
+     - Parameters:
+        - urlLink: Receives an optional URL
+        - httpMethod: ``HTTPMethod`` enum value to send data with that specific method.
+        - body: Optional raw Data for sending to remote server.
+        - jsonData: Accepts a boolean value to determine if HTTP body is in JSON format
+     
+     - Throws: An URLError is thrown if urlLink is nil or not a valied URL or server does not provide any response. Also ``HTTPStatusCode`` Error (Custom error) can be thrown if server status code is anything but 200...299
+     
+     - Returns: Returns a  ``NetworkResult<T>`` where T is raw Data
+     */
     public func fetchItemsByHTTPMethod(urlLink: URL?, httpMethod: HTTPMethod, body: Data?) async throws -> NetworkResult<Data> {
         guard let url = urlLink else {
             throw URLError(.badURL)
@@ -77,9 +128,65 @@ public class RKAPIService: RKAPIServiceProtocol {
         
         return NetworkResult(data: rawData, response: status)
     }
+}
+
+@available(iOS 9.0, *)
+@available(macOS 10.10, *)
+extension RKAPIService {
+    /**
+     Fetch items with HTTP Get method without any body parameter. Uses async/await concurrency of iOS 13
+     
+     - Parameters:
+        - urlLink: Receives an `Optional<URL>`
+        - completion: An `@escaping` closure parameter which provides a ``Result<NetworkResult<Data>, Error>`` as return of closure
+     
+     */
+    public func fetchItems(urlLink: URL?, _ completion: @escaping (Result<NetworkResult<Data>, Error>)-> Void ){
+        guard let url = urlLink else {
+            
+            completion(.failure(URLError(.badURL)))
+            
+            return
+        }
+        let request = URLRequest(url: url)
+        
+        session.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            else {
+                
+                guard let response = response as? HTTPURLResponse else {
+                    
+                    completion(.failure(URLError(.cannotParseResponse)))
+                    
+                    return
+                }
+                
+                guard let status = HTTPStatusCode(rawValue: response.statusCode) else {
+                    
+                    completion(.failure(URLError(.badServerResponse)))
+                    
+                    return
+                }
+                
+                completion(.success(NetworkResult(data: data, response: status)))
+            }
+        }
+        
+    }
     
-    @available(iOS 8.0, *)
-    @available(macOS 10.10, *)
+    /**
+     Fetch items with HTTP method with body parameter. Uses asyn/await method of iOS 13
+     
+     - Parameters:
+        - urlLink: Receives an optional URL
+        - httpMethod: ``HTTPMethod`` enum value to send data with that specific method.
+        - body: Optional raw Data for sending to remote server.
+        - jsonData: Accepts a boolean value to determine if HTTP body is in JSON format
+        - completion: An `@escaping` closure parameter which provides a ``Result<NetworkResult<Data>, Error>`` as return of closure
+     
+     */
     public func fetchItemsByHTTPMethod(urlLink: URL?, httpMethod: HTTPMethod, body: Data?, _ completion: @escaping (Result<NetworkResult<Data>, Error>)-> Void ){
         guard let url = urlLink else {
             
@@ -119,22 +226,5 @@ public class RKAPIService: RKAPIServiceProtocol {
             }
         }
         
-    }
-    
-    @available(macOS 10.15.0, *)
-    @available(iOS 13.0, *)
-    internal func previousVersionURLSession(request: URLRequest) async throws -> (Data, URLResponse) {
-        try await withCheckedThrowingContinuation({ (continuation: CheckedContinuation<(Data, URLResponse), Error>) in
-            session.dataTask(with: request) { data, response, error in
-                if let error = error {
-                 continuation.resume(throwing: error)
-                }
-                else {
-                    if let data = data, let response = response {
-                        continuation.resume(with: .success((data, response)))
-                    }
-                }
-            }
-        })
     }
 }
