dda8b515e0b62dc48d8ce2ea0fd105da002fd53f
Rakibur Khan <therakiburkhan@gmail.com>
2022-05-14 21:35:33 +0600

    Structure made frozen and added documentation

--- Sources/RKAPIService/NetworkResult.swift
+++ Sources/RKAPIService/NetworkResult.swift
@@ -5,12 +5,24 @@
 //  Created by Rakibur Khan on 2/May/22.
 //
 
-import Foundation
+import UIKit
 
-public struct NetworkResult<T> {
+/**
+ This structure is used to hold `HTTP` request response and data. It receives a generic optional paramenter for data
+ 
+ It receives a generic paramenter *`T`* as type of data which is `Optional` type. It receives ``HTTPStatusCode`` as response.
+ */
+@frozen public struct NetworkResult<T> {
     public let data: T?
     public let response: HTTPStatusCode
     
+    /**
+     Initialization
+     
+     - Parameters:
+        - data: Receives an optional value
+        - response: ``HTTPStatusCode`` enum value to send data with that specific method.
+     */
     init(data: T? = nil, response: HTTPStatusCode) {
         self.data = data
         self.response = response
